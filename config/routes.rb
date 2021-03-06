bcb0b5819a7e3b8be5e3586c8a5a207c642b16e5
EC2 Default User <ec2-user@ip-172-31-91-126.ec2.internal>
2021-03-06 04:47:25 +0000

    [Finish]応用課題5

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'search/search'
   devise_for :users
   root 'homes#top'
   get "/home/about" => "homes#about"
+  get '/search' => 'search#search'
 
   resources :users, only: [:show, :index, :edit, :update]
   resources :books, only: [:index, :show, :create, :edit, :update, :destroy] do
